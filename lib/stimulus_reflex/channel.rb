08cd4e936ae514e16bd029862b49afc62e92bd53
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-06-04 19:23:48 -0300

    fix: move monkey patch to root?

--- lib/stimulus_reflex/channel.rb
+++
@@ -1,9 +0,0 @@
-class StimulusReflex::Channel < ActionCable::Channel::Base
-  def render_page_and_broadcast_morph(reflex, selectors, data = {})
-    html = render_page(reflex)
-    if reflex.respond_to? :stimulus_controller
-      selectors = ["[data-controller=\"#{reflex.stimulus_controller}\"]"]
-    end
-    broadcast_morphs selectors, data, html if html.present?
-  end
-end
--- lib/view_component_reflex.rb
+++ lib/view_component_reflex.rb
@@ -1,7 +1,17 @@
 require "view_component_reflex/engine"
 require 'stimulus_reflex'
-require_relative 'stimulus_reflex/channel'
+require 'stimulus_reflex/channel'
 
 module ViewComponentReflex
   # Your code goes here...
+end
+
+class StimulusReflex::Channel < ActionCable::Channel::Base
+  def render_page_and_broadcast_morph(reflex, selectors, data = {})
+    html = render_page(reflex)
+    if reflex.respond_to? :stimulus_controller
+      selectors = ["[data-controller=\"#{reflex.stimulus_controller}\"]"]
+    end
+    broadcast_morphs selectors, data, html if html.present?
+  end
 end
