993680284ccfc80699b5811f6d5040a5acfcaf56
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-08-03 10:18:35 -0300

    fix: import order

--- lib/view_component_reflex.rb
+++ lib/view_component_reflex.rb
@@ -1,8 +1,8 @@
 require "view_component_reflex/state_adapter/session"
 require "view_component_reflex/state_adapter/memory"
-require "view_component_reflex/reflex"
 require "view_component_reflex/engine"
 require "stimulus_reflex"
+require "view_component_reflex/reflex"
 
 module ViewComponentReflex
   # Your code goes here...
