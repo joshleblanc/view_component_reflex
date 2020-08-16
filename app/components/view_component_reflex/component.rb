43123661a2e1e19013c22b25016cdc0f1c0fe6f4
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-08-16 11:51:29 -0300

    chore: lint

--- app/components/view_component_reflex/component.rb
+++ app/components/view_component_reflex/component.rb
@@ -2,10 +2,10 @@ module ViewComponentReflex
   class Component < ViewComponent::Base
     class << self
       def init_stimulus_reflex
-        if name.include? "::"
-          @stimulus_reflex ||= module_parent.const_set(name.split("::").last + "Reflex", Class.new(Reflex))
+        @stimulus_reflex ||= if name.include? "::"
+          module_parent.const_set(name.split("::").last + "Reflex", Class.new(Reflex))
         else
-          @stimulus_reflex ||= Object.const_set(name + "Reflex", Class.new(Reflex))
+          Object.const_set(name + "Reflex", Class.new(Reflex))
         end
         @stimulus_reflex.component_class = self
       end
