ad112abbd79d9bf449bb13c5756c370f68bdba70
Joshua LeBlanc <jleblanc@omics.com>
2020-09-23 13:00:06 -0300

    fix: remove xxhash

--- app/components/view_component_reflex/component.rb
+++ app/components/view_component_reflex/component.rb
@@ -100,7 +100,7 @@ module ViewComponentReflex
       Rails.logger.info "Generating key for #{self.class.name}"
       Rails.logger.info "file: #{erb_file}"
       key = if erb_file
-        XXhash.xxh32(erb_file.split(":in")[0]).to_s
+        Digest::SHA2.hexdigest(erb_file.split(":in")[0])
       else
         ""
       end
