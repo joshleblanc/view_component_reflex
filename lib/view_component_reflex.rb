b6fcbe4468ce58a226216be40ef207eb70d0469a
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-10-17 07:29:35 -0300

    fix: import redis adapter

--- lib/view_component_reflex.rb
+++ lib/view_component_reflex.rb
@@ -2,6 +2,7 @@ require "stimulus_reflex"
 require 'view_component_reflex/reflex_factory'
 require "view_component_reflex/state_adapter/session"
 require "view_component_reflex/state_adapter/memory"
+require "view_component_reflex/state_adapter/redis"
 require "view_component_reflex/reflex"
 require "view_component_reflex/engine"
 
