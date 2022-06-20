5c876b9bec60aebdda470e45bad3569846f9131d
Josh LeBlanc <jleblanc@hey.com>
2022-06-20 11:47:52 +0000

    fix: dom state key could get manipulated when inserted into html

--- lib/view_component_reflex/component.rb
+++ lib/view_component_reflex/component.rb
@@ -188,12 +188,13 @@ module ViewComponentReflex
       # and line number, which should be unique. We hash it to make it a nice number
       erb_file = caller.select { |p| p.match? /.\.html\.(haml|erb|slim)/ }[1]
       key = if erb_file
-              Digest::SHA2.hexdigest(erb_file.split(":in")[0])
+              erb_file.split(":in")[0]
+              
             else
               ""
             end
       key += collection_key.to_s if collection_key
-      @key = key
+      @key = Digest::SHA2.hexdigest(key)
     end
 
     # Helper to use to create the proper reflex data attributes for an element
